afa02c6a0c3f6a93607e1dad636c48de59e65fec
Alale Clifford <110997402+Alalecl@users.noreply.github.com>
2022-09-13 19:10:25 -0700

    co

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int num_1 = 1, num_2 = 2, next = 0, sum = num_2;
-	while (nxt <= 4000000)
+	while (next <= 4000000)
 	{
 		next = num_1 + num_2;
 		num_1 = num_2;
@@ -17,7 +17,7 @@ int main(void)
 		if (next % 2 == 0)
 			sum += next;
 	}
-	printf("%\n", sum);
+	printf("%i\n", sum);
 
 	return (0);
 }
