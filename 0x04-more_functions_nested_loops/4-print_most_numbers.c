17a3ef66d733cd24d2a2053df82f81696ba98e6c
Alale Clifford <110997402+Alalecl@users.noreply.github.com>
2022-09-15 11:50:13 -0700

    gi

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
 
 void print_most_numbers(void)
 {
-	int i;
-	for (i = 0; i < 10; i++)
-	{
-		if (i == 2 || i == 4)
-			continue;
-		_putchar(i);
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i == 2 || i == 4)
+continue;
+_putchar(i);
+}
+_putchar('\n');
 }
